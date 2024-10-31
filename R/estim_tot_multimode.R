6e64834ab31960c2dc5090f9c9e56829f4dab86f
Tangi Tassin <dev.ttassin@proton.me>
2024-10-31 16:33:44 +0100

    Added variances calculation for internet, telephone and difference test (for hypothesis test)

--- R/estim_tot_multimode.R
+++ R/estim_tot_multimode.R
@@ -201,18 +201,140 @@
 #   crossprod(Y, 1.0 / invWeights)
 # }
 
+
+.bPhi11 <- function(expY1, I, R1, p1, Z,
+                    phi = rep(1.0, length(expY1)),
+                    constY = FALSE)
+{
+
+  weightedY1 <- phi * expY1
+
+
+  # muPhi11 (expected value of the derivative of the total
+  # with alpha_1 = alpha_1^*) depends on Y1, so if the y_1k are
+  # considered as constant then we can only use Sr.
+  if (constY1)
+  {
+    p1Sr <- p1[R1]
+    weightedY1Sr <- weightedY1[R1]
+    muPhi11 <- crossprod(2.0 * weightedY1Sr *
+                           (1.0 - p1Sr) / (piSr * p1Sr^2L),
+                         Z[R1, , drop = FALSE])
+  }
+  else
+  {
+    muPhi11 <- crossprod(2.0 * weightedY1 *
+                           (1.0 - p1) / p1, Z)
+  }
+
+
+  # diagonal matrix of ^p_1k * (1-^p1k)
+  lambda1S <- diag(p1[I] * (1.0 - p1[I]) / pi[I])
+  partialW1 <- t(Z[I, , drop = FALSE]) %*%
+    lambda1S %*%
+    Z[I, , drop = FALSE]
+
+  bPhi11 <- t(muPhi11) %*% solve(partialW1)
+
+  return(bPhi11)
+}
+
+
+.bPhi21 <- function(expY2, I, p1, R2, p2, Z,
+                    phi = rep(1.0, length(expY1)),
+                    constY = FALSE)
+{
+
+  weightedY2 <- phi * expY2
+
+  p1Bar <- 1.0 - p1
+
+  # muPhi21 (expected value of the derivative of the total
+  # with alpha_1 = alpha_1^*) depends on Y2, so if the y_2k are
+  # considered as constant then we can only use Smr.
+  if (constY2)
+  {
+    p1BarSmr <- p1Bar[R2]
+    p2Smr <- p2[R2]
+    weightedY2Smr <- weightedY2[R2]
+
+    muPhi21 <- crossprod(2.0 * weightedY2Smr *
+                           p1Smr / (piSmr * p1BarSmr^2L * p2Smr),
+                         Z[R2, , drop = FALSE])
+  }
+  else
+  {
+    muPhi21 <- crossprod(2.0 * weightedY2 * p1 / p1Bar, Z)
+  }
+
+
+  # diagonal matrix of ^p_1k * (1-^p1k)
+  lambda1S <- diag(p1[I] * (1.0 - p1[I]) / pi[I])
+  partialW1 <- t(Z[I, , drop = FALSE]) %*%
+    lambda1S %*%
+    Z[I, , drop = FALSE]
+
+  bPhi21 <- t(muPhi21) %*% solve(partialW1)
+
+  return(bPhi21)
+}
+
+
+.bPhi22 <- function(expY2, I, R1, p1, R2, p2, Z,
+                    phi = rep(1.0, length(expY1)),
+                    constY = FALSE)
+
+{
+
+  weightedY2 <- phi * expY2
+
+  p1Bar <- 1.0 - p1
+
+  # muPhi22 (expected value of the derivative of the total
+  # with alpha_2 = alpha_2^*) depends on Y2, so if the y_2k are
+  # considered as constant then we can only use Smr.
+  if (constY2)
+  {
+    p1BarSmr <- p1Bar[R2]
+    p2Smr <- p2[R2]
+    weightedY2Smr <- weightedY2[R2]
+
+    muPhi22 <- crossprod(2.0 * weightedY2Smr *
+                           (1.0 - p2Smr) / (piSmr * p1BarSmr * p2Smr^2L),
+                         Z[R2, , drop = FALSE])
+  }
+  else
+  {
+    muPhi22 <- crossprod(2.0 * weigtedY2 * (1.0 - p2) / p2, Z)
+  }
+
+  indicatorSm <- I & !R1
+  # diagonal matrix of ^p_1k * (1-^p1k)
+  lambda2Sm <- diag(p2[indicatorSm] * (1.0 - p2[indicatorSm]) /
+                      (pi[indicatorSm] * p1Bar[indicatorSm]))
+  partialW2 <- t(Z[indicatorSm, , drop = FALSE]) %*%
+    lambda2Sm %*%
+    Z[indicatorSm, , drop = FALSE]
+
+  bPhi22 <- t(muPhi22) %*% solve(partialW2)
+
+}
+
 #' Calculate the true variance of the HT estimator of t_phi1 in the case
 #' of known selection probabilities. Using estimated values give an approximate
 #' variance in the case of known selection probabilities
 #' @export
-var_HT_seq_phi1 <- function(expY1, covarY1,
+var_HT_seq_phi1 <- function(expY1,
+                            I,
                             piMat,
                             pq1Mat,
-                            phi = rep(1.0, length(expY2)),
-                            correcEstimWeights = FALSE,
                             Z,
                             biasedMode,
-                            modes)
+                            modes,
+                            phi = rep(1.0, length(expY1)),
+                            correcEstimWeights = FALSE,
+                            constY1 = FALSE,
+                            covarY1 = NULL)
 {
 
   if (all(phi == 0.0))
@@ -223,49 +345,80 @@ var_HT_seq_phi1 <- function(expY1, covarY1,
   p1 <- diag(pq1Mat)
   invP1Mat <- (p1 %*% t(p1))^-1L
 
-  weightedPhis <- phi / pi
+  R1 <- as.numeric(modes == biasedMode)
 
-  # Y_1 variability
-  varY1 <- piMat * pq1Mat * covarY1 * invP1Mat
-  varY1 <- weightedPhis %*% t(weightedPhis) * varY1
+  covarPi <- pi2_to_covarInc(piMat)
 
-  # mode 1 selection variability
-  if (correcEstimWeights)
+  weightedY1 <- phi * expY1
+
+
+  # Sampling variability (S)
+
+  # If Y1 is considered as constant we can only estimate the sub-variance
+  # in Sr
+  if (constY1)
   {
+    piSr <- pi[R1]
+    weightedY1Sr <- weightedY1[R1]
+    varS <- weightedY1Sr / piSr %*% t(weightedY1Sr / piSr) *
+      covarPi[R1, R1] /
+      (piMat[R1, R1] * pq1Mat[R1, R1])
+  }
+  # Otherwise we can do it on U
+  else
+    varS <- weightedY1 / pi %*% t(weightedY1 / pi) * covarPi
 
-    inverse1 <- solve(t(Z) %*%
-                        diag(p1 * (1.0 - 2.0 * p1) / (1.0 - p1)^2L) %*% Z)
+  # q1 variability (R1)
+  # If we use estimated p_1k weights we have to do a correction.
 
-    indicator1 <- as.numeric(modes == biasedMode)
-    U1 <- inverse1 %*% t(Z) %*% diag(indicator1 - p1)
-    rm(inverse1)
+  covarq1 <- pi2_to_covarInc(pq1Mat)
 
-    muphi11 <- 2.0 * crossprod(phi * expY1 * (p1^-1L - 1.0), Z)
+  if (constY1)
+  {
+    correctedY1Sr <- weightedY1Sr / p1Sr
 
-    X <- (phi * expY1 + as.vector(muphi11 %*% U1)) / pi
 
-    rm(U1)
+    if (correcEstimWeights)
+    {
+      bPhi11 <- .bPhi11(expY1, I, R1, p1, Z, phi, constY)
+      correctedY1Sr <- correctedY1Sr -
+        crossprod(Z[R1, , drop = FALSE], bPhi11)
+    }
+
+    varq1 <- (correctedY1Sr / piSr) %*% t(correctedY1Sr / piSr) *
+      covarq1[R1, R1] / pq1Mat[R1, R1]
   }
   else
   {
-    X <- weightedPhis * expY1
+    correctedY1 <- weightedY1 / p1
+
+    if (correcEstimWeights)
+    {
+      bPhi11 <- .bPhi11(expY1, I, R1, p1, Z, phi, constY)
+      correctedY1 <- correctedY1Sr - crossprod(Z, bPhi11)
+    }
+
+    varq1 <- (correctedY1 / pi) %*% t(correctedY1 / pi) *
+      piMat * covarq1
   }
 
-  prodexpY1Mat <- X %*% t(X)
-  rm(X)
+  var <- sum(varS + varq1)
+
+  # If the y_1k are constants the complete
+  # variance has been calculated
+  # Otherwise is missing the part of variability from y_1k
+  if (constY1)
+    return(var)
+
+  # Y1 variability
+
+  # Can be calculated on the entire population U
+  variables <- phi / (pi * p1)
+
+  varY1 <- variables %*% t(variables) * piMat * pq1Mat * covarY1
 
 
-
-
-  covarq1 <- pi2_to_covarInc(pq1Mat)
-  varq1 <- prodexpY1Mat * piMat * covarq1 * invP1Mat
-
-  # sampling variability
-  covarPi <- pi2_to_covarInc(piMat)
-  varS <- prodexpY1Mat * covarPi
-
-  sum(varY1 + varq1 + varS)
-
+  var + sum(varY1)
 }
 
 
@@ -274,23 +427,22 @@ var_HT_seq_phi1 <- function(expY1, covarY1,
 #' Using estimated values give an approximate variance in the case of
 #'  known selection probabilities.
 #' @export
-var_HT_seq_phi2 <- function(expY2, covarY2,
+var_HT_seq_phi2 <- function(expY2, I,
                             piMat,
                             pq1Mat, pq2Mat,
-                            phi = numeric(length(expY2)),
-                            correcEstimWeights = FALSE,
                             Z,
                             biasedMode,
                             refMode,
-                            modes)
+                            modes,
+                            phi = rep(1.0, length(expY2)),
+                            correcEstimWeights = FALSE,
+                            constY2 = FALSE,
+                            covarY2 = NULL)
 {
 
-  expit <- function(x) 1.0 / (1.0 + exp(-x))
-
-  if (all(phi == 1.0))
+  if (all(phi == 0.0))
     return(0.0)
 
-  N <- length(expY2)
 
   pi <- diag(piMat)
 
@@ -304,80 +456,281 @@ var_HT_seq_phi2 <- function(expY2, covarY2,
   p1Bar <- 1.0 - p1
 
   p2 <- diag(pq2Mat)
-  p2Bar <- 1.0 - p2
 
-  invp1BarMat <- (p1Bar %*% t(p1Bar))^-1L
-  invProbsMatSelecMat <- invp1BarMat * (p2 %*% t(p2))^-1L
+  R1 <- as.numeric(modes == biasedMode)
+  R2 <- as.numeric(modes == refMode)
 
-  phiBar <- 1.0 - phi
-  weightedPhis <- phiBar / pi
+  weightedY2 <- phi * expY2
 
-  ##browser()
 
-  # Y_2 variability
-  varY2 <- piMat *
-    pq1BarMat *
-    pq2Mat *
-    covarY2 *
-    invProbsMatSelecMat
+  # Sampling variability (S)
 
-  varY2 <- weightedPhis %*% t(weightedPhis) * varY2
-
-  if (correcEstimWeights)
+  # If Y2 is considered as constant we can only estimate the sub-variance
+  # in Sr
+  if (constY2)
   {
-    inverse1 <- solve(t(Z) %*% diag(p1 * (1.0 - 2.0 * p1) / p1Bar^2L) %*% Z)
-    indicator1 <- as.numeric(modes == biasedMode)
-    U1 <- inverse1 %*% t(Z) %*% diag(indicator1 - p1)
-    rm(inverse1)
+    piSmr <- pi[R2]
+    weightedY2Smr <- weightedY2[R2]
+    piMatSmr <- piMat[R2, R2]
+    pq1BarMatSmr <- pq1BarMat[R2, R2]
+    pq2MatSmr <- pq2Mat[R2, R2]
 
-    muphi21 <- 2.0 * crossprod(phiBar * expY2 * p1 / p1Bar, Z)
+    varS <- weightedY2Smr / piSmr %*% t(weightedY2Smr / piSmr) *
+      covarPi[R2, R2] /
+      (piMatSmr * pq1BarMatSmr * pq2MatSmr)
+  }
+  # Otherwise we can do it on U
+  else
+  {
+    varS <- weightedY1 / pi %*% t(weightedY1 / pi) * covarPi
+  }
 
-    inverse2 <- solve(t(Z) %*% diag(p2 * (1.0 - 2.0 * p2) / p2Bar^2L) %*% Z)
-    indicator2 <- as.numeric(modes == refMode)
 
-    U2 <- inverse2 %*% t(Z) %*% diag(indicator2 - p2)
-    rm(inverse2)
+  # q1 variability (R1)
+  # If we use estimated p_1k weights we have to do a correction.
 
-    muphi22 <- 2.0 * crossprod(phiBar * expY2 * (p2^-1L - 1.0), Z)
+  covarq1 <- pi2_to_covarInc(pq1Mat)
 
-    X <- (phiBar * expY2 +
-            as.vector(muphi21 %*% U1) +
-            as.vector(muphi22 %*% U2)) / pi
+  if (constY2)
+  {
+    correctedY2Smr <- weightedY2Smr / p1BarSmr
 
-    rm(U1, U2)
 
+    if (correcEstimWeights)
+    {
+      bPhi21 <- .bPhi21(expY2, I, p1, R2, p2, Z, phi, constY)
+      correctedY2Smr <- correctedY2Smr +
+        crossprod(Z[R2, , drop = FALSE], bPhi21)
+    }
+
+    varq1 <- (correctedY2Smr / piSmr) %*% t(correctedY2Smr / piSr) *
+      covarq1[R2, R2] /
+      pq1Mat[R2, R2]
   }
   else
   {
-    X <- weightedPhis * expY2 / pi
+    correctedY2 <- weightedY2 / p1Bar
+
+    if (correcEstimWeights)
+    {
+      bPhi21 <- .bPhi21(expY2, I, p1, R2, p2, Z, phi, constY)
+      correctedY2 <- correctedY2 + crossprod(Z, bPhi21)
+    }
+
+    varq1 <- (correctedY2 / pi) %*% t(correctedY2 / pi) *
+      piMat * covarq1
   }
 
-  prodexpY2Mat <- X %*% t(X)
-  rm(X)
+  # q2 variability (R2)
+  # If we use estimated p_2k weights we have to do a correction.
 
-  # mode 2 selection variability
+
   covarq2 <- pi2_to_covarInc(pq2Mat)
-  varq2 <- prodexpY2Mat *
-    piMat *
-    pq1BarMat *
-    covarq2 *
-    invProbsMatSelecMat
+
+  if (constY2)
+  {
+    correctedY2Smr <- weightedY2Smr / p2Smr
+
+    if (correcEstimWeights)
+    {
+      bPhi22 <- .bPhi22(expY2, I, R1, p1, R2, p2, Z, phi, constY)
+      correctedY2Smr <- correctedY2Smr -
+        crossprod(Z[R2, , drop = FALSE], bPhi22)
+    }
+
+    varq2 <- (correctedY2Smr / (piSmr * p1BarSmr)) %*%
+      t(correctedY2Smr / (piSmr * p1BarSmr)) *
+      pq1BarMat[R2, R2] *
+      covarq2[R2, R2] /
+      pq1Mat[R2, R2]
+  }
+  else
+  {
+    correctedY2 <- weightedY2 / p2
+
+    if (correcEstimWeights)
+    {
+      bPhi22 <- .bPhi22(expY2, I, R1, p1, R2, p2, Z, phi, constY)
+      correctedY2 <- correctedY2 - crossprod(Z, bPhi22)
+    }
+
+    varq2 <- (correctedY2 / (pi * p1Bar)) %*% t(correctedY2 / (pi * p1Bar)) *
+      piMat * pq1BarMat * covarq2
+  }
+
+  var <- sum(varS + varq1 + varq2)
+
+  # If the y_2k are constants the complete
+  # variance has been calculated
+  # Otherwise is missing the part of variability from y_2k
+  if (constY2)
+    return(var)
+
+  # Y2 variability
+
+  variables <- phi / (pi * p1Bar * p2)
+
+  varY2 <- variables %*% t(variables) * piMat * pq1Mat * pq2Mat * covarY1
 
 
-  # mode 1 selection variability
-  covarq1 <- pi2_to_covarInc(pq1Mat)
-  varq1 <- prodexpY2Mat *
-    piMat *
-    covarq1 *
-    invp1BarMat
+  var + sum(varY2)
 
-  # sampling variability
+}
+
+covar_difference_HT <- function(expY1, expY2, I,
+                                piMat,
+                                pq1Mat, pq2Mat,
+                                Z,
+                                biasedMode,
+                                refMode,
+                                modes,
+                                phi = rep(1.0, length(expY1)),
+                                correcEstimWeights = FALSE,
+                                constY = FALSE,
+                                covarY1 = NULL,
+                                covarY2 = NULL)
+{
+  if (all(phi == 0.0))
+    return(0.0)
+
+  weightedY1 <- phi * expY1
+  weightedY2 <- phi * expY2
+
+  # Sampling variability (S)
   covarPi <- pi2_to_covarInc(piMat)
-  varS <- prodexpY2Mat * covarPi
+  if (constY)
+  {
+    ## TO DO
+  }
+  else
+  {
+    varS <- weightedY1 / pi %*% t(weightedY2 / pi) * covarPi
+  }
 
-  vMat <- varY2 + varq2 + varq1 + varS
+  # q1 variability (R1)
 
-  sum(vMat)
+  covarq1 <- pi2_to_covarInc(pq1Mat)
+  if (constY)
+  {
+    ## TO DO
+  }
+  else
+  {
+
+    correctedY1 <- weightedY1 / p1
+
+    if (correcEstimWeights)
+    {
+      bPhi11 <- .bPhi11(expY1, I, R1, p1, Z, phi, constY)
+      correctedY1 <- correctedY1Sr - crossprod(Z, bPhi11)
+    }
+
+    correctedY2 <- weightedY2 / p1Bar
+
+    if (correcEstimWeights)
+    {
+      bPhi21 <- .bPhi21(expY2, I, p1, R2, p2, Z, phi, constY)
+      correctedY2 <- correctedY2 + crossprod(Z, bPhi21)
+    }
+
+    varq1 <- -(correctedY1 / pi) %*% t(correctedY2 / pi) * piMat * covarq1
+  }
+
+
+  sum(varS + varq1)
+}
+
+var_difference_HT <- function(expY1, expY2, I,
+                              piMat,
+                              pq1Mat, pq2Mat,
+                              Z,
+                              biasedMode,
+                              refMode,
+                              modes,
+                              phi = rep(1.0, length(expY2)),
+                              correcEstimWeights = FALSE,
+                              constY = FALSE,
+                              covarY1 = NULL,
+                              covarY2 = NULL)
+{
+  var_HT_seq_phi1(expY1, I, piMat, pq1Mat, Z, biasedMode,
+                  modes, phi, correcEstimWeights, constY1, covarY1) -
+    2.0 * covar_difference_HT(expY1, expY2, I,
+                              piMat,
+                              pq1Mat, pq2Mat,
+                              Z,
+                              biasedMode,
+                              refMode,
+                              modes,
+                              phi,
+                              correcEstimWeights,
+                              constY,
+                              covarY1,
+                              covarY2) +
+    var_HT_seq_phi2(expY2, I, piMat, pq1Mat, pq2Mat, Z, biasedMode,
+                    refMode, modes, phi, correcEstimWeights,
+                    constY2, covarY2)
+}
+
+test_absence_measure_bias <- function(expY1, expY2, I,
+                                      piMat,
+                                      pq1Mat, pq2Mat,
+                                      Z,
+                                      biasedMode,
+                                      refMode,
+                                      modes,
+                                      phi = rep(1.0, length(expY2)),
+                                      correcEstimWeights = FALSE,
+                                      constY = FALSE,
+                                      covarY1 = NULL,
+                                      covarY2 = NULL,
+                                      alpha = 0.05)
+{
+
+  pi <- diag(piMat)
+
+
+  R1 <- as.numeric(modes == biasedMode)
+  p1 <- diag(pq1Mat)
+
+  tPhi1 <- sum(phi[R1] * expY1[R1] / (pi[R1] * p1[R1]))
+
+  R2 <- as.numeric(modes == refMode)
+  p2 <- diag(pq2Mat)
+
+  tPhi2 <- sum(phi[R2] * expY2[R2] / (pi[R2] * (1.0 - p1[R2]) * p2[R2]))
+
+  delta <- tPhi1 - tPhi2
+
+  varDelta <- var_difference_HT(expY1, expY2, I,
+                                piMat,
+                                pq1Mat, pq2Mat,
+                                Z,
+                                biasedMode,
+                                refMode,
+                                modes,
+                                phi,
+                                correcEstimWeights,
+                                constY,
+                                covarY1,
+                                covarY2)
+
+  Zstat <- delta / sqrt(varDelta)
+
+  pValue <- 2.0 * (1.0 - pnorm(Zstat))
+
+  uAlpha <- qnorm(1.0 - alpha / 2.0)
+
+  if (pValue <= alpha)
+    finalHyp <- "H0 (no measure bias)"
+  else
+    finalHyp <- "H1 (at least one measure bias)"
+
+  results <- c(Z = Zstat, alpha = alpha,
+               pValue = pValue, uAlpha = uAlpha,
+               hypothesis = finalHyp)
+
+  results
 }
 
 #' @export
